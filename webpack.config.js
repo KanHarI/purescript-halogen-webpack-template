9937efd0830f8d0717087670a778f501ba43fa1d
Itay KanHar <knaan.harpaz@gmail.com>
2021-01-01 19:25:39 +0200

    Solve seperate serving and bundling

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const path = require("path");
 
 module.exports = {
-    entry: "./dev/loader.js",
+    entry: this.mode == "production" ? "./dev/loader_prod.js" : "./dev/loader_dev.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, 'dist')
