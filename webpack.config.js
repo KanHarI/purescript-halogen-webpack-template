ee9546483599367f66a93c593407e9e4c348965f
Itay KanHar <knaan.harpaz@gmail.com>
2021-01-01 18:31:31 +0200

    Linting

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         }),
         new CopyPlugin({
             patterns: [
-                { from: "public", to: "."}
+                {from: "public", to: "."}
             ]
         })
     ]
